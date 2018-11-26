a070c346097181a4c6e98f9f865c82d475628018
hendrikniemann <hendrik.niemann@kiron.ngo>
2018-11-27 00:55:41 +0100

    Use newest argonaut codec version
    This adds a new combinators and the function getFieldOptional' for
    decoding JSONs that contain either undefined or null when the field is
    not present. This is important because some GraphQL clients send
    { operationName: null } inside of the body of the request.

--- src/Main.purs
+++ src/Main.purs
@@ -2,38 +2,40 @@ module Main where
 
 import Prelude
 
-import Data.Argonaut (class DecodeJson, Json, decodeJson, jsonParser, stringify, (.?), (.??))
+import Context (Context)
+import Data.Argonaut.Core (Json, stringify)
+import Data.Argonaut.Decode (decodeJson, (.:), (.:?))
+import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either(..), either)
 import Data.Maybe (Maybe)
 import Effect (Effect)
 import Effect.Aff (runAff_)
 import Effect.Class (liftEffect)
 import Effect.Console as Console
-import Context (Context)
 import GraphQL (graphql)
 import HTTPure as HTTPure
 import Schema (schema)
 import Store (createStore)
 
-newtype GraphQLParams = GraphQLParams
+type GraphQLParams =
   { query :: String
   , variables :: Maybe Json
   , operationName :: Maybe String
   }
 
-instance decodeJsonGraphQLParams :: DecodeJson GraphQLParams where
-  decodeJson json = do
-    obj <- decodeJson json
-    query <- obj .? "query"
-    variables <- obj .?? "variables"
-    operationName <- obj .?? "operationName"
-    pure $ GraphQLParams { query, variables, operationName }
+decodeParams :: Json -> Either String GraphQLParams
+decodeParams json = do
+  obj <- decodeJson json
+  query <- obj .: "query"
+  variables <- obj .:? "variables"
+  operationName <- obj .:? "operationName"
+  pure $ { query, variables, operationName }
 
 createRouter :: Context -> HTTPure.Request -> HTTPure.ResponseM
 createRouter context { body, method: HTTPure.Post, path: [ "graphql" ] } =
-  case jsonParser body >>= decodeJson of
+  case jsonParser body >>= decodeParams of
     Left error -> HTTPure.badRequest error
-    Right (GraphQLParams { query, variables, operationName }) -> do
+    Right { query, variables, operationName } -> do
       result <- graphql schema query unit context variables operationName
       HTTPure.ok $ stringify result
 createRouter _ _ = HTTPure.notFound
